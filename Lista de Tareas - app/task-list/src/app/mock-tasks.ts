3cd8f92d9421175e7ed26cc5aa4f0032a34530b9
jkraft <jkraft@rfindustrial.com>
2022-02-20 01:30:42 -0300

    AgregoAppTask
    Aplicacion de tareas en Angular, funcionando y lista para ser mejorada

---
+++ Lista de Tareas - app/task-list/src/app/mock-tasks.ts
@@ -0,0 +1,29 @@
+import {Task} from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar el primer modulo de Angular",
+        day: 'Agosto 5 a las 12:19',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "HAcer las compras de materiales",
+        day: 'Febrero 21 a las 13:05',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Lavar la camioneta completa",
+        day: 'Marzo 11 a las 08:00',
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "Terminar habitacion de la bb",
+        day: 'Abril 1 a las 00:00',
+        reminder: true
+    }
+
+]
